8b9818f0ec991c75d480e5f18cf61b796ec7efce
githubShraddha26 <shraddharaghav26@gmail.com>
2024-05-28 21:15:02 +0530

    Namaste React Episode 1

---
+++ App.js
@@ -0,0 +1,22 @@
+const parent = React.createElement("div",{id:"parent"},
+    [
+    React.createElement("div",{id:"child"},
+    React.createElement("h1",{},"h1 Tag"),
+    React.createElement("h1",{},"h1 Tag"),
+    ),
+    React.createElement("div",{id:"child2"},
+    React.createElement("h1",{},"h1 Tag"),
+    React.createElement("h1",{},"h1 Tag"),
+    )
+   ]
+)
+
+const heading = React.createElement("h1",{id:'heading'},"Hello World from React")
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent);
+
+console.log(parent);
+
+
